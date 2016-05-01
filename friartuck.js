ba90cda1ee8aee82b2611f5972532092d2d2158d
Busse <chrisbusse@gmail.com>
2016-04-30 21:48:01 -0400

    have it to the point where I need to test when market is open

--- friartuck.js
+++ friartuck.js
@@ -1,6 +1,15 @@
 var robinhood = require('robinhood');
 var commandLineArgs = require('command-line-args'); // https://www.npmjs.com/package/command-line-args
 
+
+//variables
+
+var R
+var BP
+var LTP
+var INSTRUMENT
+var prep_options
+
 function login () {
   return new Promise((resolve, reject) => {
     //prompt.message = prompt.delimeter = ''
@@ -15,11 +24,11 @@ function login () {
     //     reject(err)
     //   } else {
     //     R = robinhood({ username: res.username, password: res.password }, resolve)
-    	   R = robinhood({ username: options['username'], password: options['password'] }, resolve)
+    	   R = robinhood({ username: cli_options['username'], password: cli_options['password'] }, resolve)
     //   }
     // })
 
-	console.log(options['action']);
+	console.log(cli_options['action']);
   })
 }
 
@@ -29,7 +38,7 @@ function getOrders () {
       if (err) {
         reject(err)
       } else {
-      	console.log(body.results);
+      	console.log(body);
         resolve(body.results)
       }
     })
@@ -55,12 +64,80 @@ function getBuyingPower () {
       if (err) {
         reject(err)
       } else {
-      	console.log(body.results[0]['buying_power']);
+      	//console.log(body.results)
+      	BP = body.results[0]['buying_power']
         resolve(body.results[0]['buying_power'])
       }
     })
   )
 }
+
+function getInstrumentURI () {
+  return new Promise((resolve, reject) =>
+    R.instruments(cli_options['symbol'], (err, res, body) => {
+      if (err) {
+        reject(err)
+      } else {
+      	INSTRUMENT = body.results[0].url
+        console.log(body.results[0].url)
+        resolve(body.results[0].url)
+      }
+    })
+  )
+}
+
+function getDrinkSize (buying_power) {
+  return new Promise((resolve, reject) =>
+    R.quote_data(cli_options['symbol'], (err, res, body) => {
+      if (err) {
+        reject(err)
+      } else {
+      	//console.log(body.results[0]['buying_power'])
+        //resolve(body.results[0]['buying_power'])
+        LTP = parseFloat(body.results[0]['last_trade_price'])
+        var stb = buying_power / LTP
+        var wholestb = Math.floor(stb)
+        console.log('Math.floor(' + buying_power + '/' + body.results[0]['last_trade_price'] + '=' + stb + ')=' + wholestb)
+        resolve(wholestb)
+      }
+    })
+  )
+}
+
+function prepBuyOrder(drink_size) {
+	var buy_options = {
+	    bid_price: LTP,
+	    quantity: drink_size,
+	    instrument: {
+	        url: INSTRUMENT,
+	        symbol: cli_options['symbol']
+	     }
+	    // },
+	    // // Optional:
+	    // 'trigger': String, // Defaults to "gfd" (Good For Day)
+	    // 'time': String,    // Defaults to "immediate"
+	    // 'type': String     // Defaults to "market"
+	}
+	return buy_options
+}
+
+function postBuyOrder (buy_options) {
+  return new Promise((resolve, reject) =>
+    R.place_buy_order(buy_options, (err, res, body) => {
+      if (err) {
+        reject(err)
+      } else {
+        console.log(body.results)
+        resolve(body.results)
+      }
+    })
+  )
+}
+
+function showFinalResults (buy_options) {
+  console.log(buy_options)
+}
+
 
 console.log("friartuck wakes up");
 
@@ -68,18 +145,31 @@ var cli = commandLineArgs([
   { name: 'username', alias: 'u', type: String },
   { name: 'password', alias: 'p', type: String },	
   { name: 'action', alias: 'a', type: String },
-  { name: 'passout', type: String },
   { name: 'symbol', alias: 's', type: String },
   { name: 'stop-loss-percent', alias: 'l', type: Number },
   { name: 'limit-percent', alias: 'h', type: Number }
 ])
 
-var options = cli.parse();
+var cli_options = cli.parse();
 
 //console.log(cli.getUsage());
+// login()
+// .then(getBuyingPower)
+// .then(console.log("There's this much ale to drink: " + BP))
+// .catch(err => { console.error(err); process.exit(1) })
+//console.log(cli_options['action']);
+
+
+
+// login()
+// .then(getInstrumentURI)
+// .then(getBuyingPower)
+// .then(getDrinkSize)
+// .then(prepBuyOrder)
+// //.then(postBuyOrder)
+// .then(showFinalResults)
 login()
-.then(getBuyingPower)
-.catch(err => { console.error(err); process.exit(1) })
-//console.log(options['action']);
+.then(getOrders)
+//.then(console.log("There's this much ale to drink: " + BP))
 
 
